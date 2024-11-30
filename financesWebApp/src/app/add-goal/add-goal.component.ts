926aee573e3a2c93e9543a9cb246e3f601c546eb
icc10 <99215615+icc10@users.noreply.github.com>
2024-11-30 17:27:35 -0500

    made add goal HTML

--- financesWebApp/src/app/add-goal/add-goal.component.ts
+++ financesWebApp/src/app/add-goal/add-goal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Goal } from '../models/Goal';
 
 @Component({
   selector: 'app-add-goal',
@@ -9,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AddGoalComponent {
   submitted: boolean = false;
+
+  goal: Goal = {
+    name: '',
+    amountToSave: '',
+    date: '',
+    amountSaved: '',
+  };
+
+  newGoal(): void {
+    const data = {
+      name: this.goal.name,
+      amountToSave: this.goal.amountToSave,
+      date: this.goal.date,
+      amountSaved: this.goal.amountSaved
+    };
+
+    console.log(data);
+  }
 }
